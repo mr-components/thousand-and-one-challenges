1be1b1a908ce3862f126355fa6c785d0285cea8f
Luis <Luis@LAPTOP-0EFHT9GS>
2021-12-29 23:41:04 -0500

    Create saveData script

--- src/tools/code-writer/index.js
+++ src/tools/code-writer/index.js
@@ -1,6 +1,8 @@
 const { chromium } = require('playwright')
 const { prompt } = require('inquirer');
 const { loginWithEnvVariables } = require('./config.json')
+const { mkdir } = require('fs/promises');
+const { appendFileSync } = require('fs');
 require('dotenv').config()
 
 // Función encargada de obtener los códigos
@@ -31,7 +33,6 @@ const obtainData = async page => {
 }
 
 const obtainGitHubCredentials = async () => {
-  console.log(loginWithEnvVariables);
   if(loginWithEnvVariables) {
     const { USER_GITHUB_MAIL, USER_GITHUB_PASSWORD } = process.env
     return {
@@ -60,7 +61,7 @@ const obtainGitHubCredentials = async () => {
 
 const login = async page => {
   const { email, password } = await obtainGitHubCredentials()
-  console.log('Vamos a iniciar sesión en GitHub');
+  console.log('Iniciando sesión en GitHub...');
   const emailInput = await page.locator('#login_field')
   const passwordInput = await page.locator('#password')
   const signInButton = await page.locator('[name="commit"]')
@@ -70,7 +71,7 @@ const login = async page => {
 }
 
 const getInfo = async () => {
-  console.log('Iniciando scrapping');
+  console.log('Iniciando scrapping...');
   const browser = await chromium.launch({
     headless: true
   }) 
@@ -81,12 +82,49 @@ const getInfo = async () => {
   await login(adventJsPage)
   console.log('Esperando respuesta de GH...');
   await adventJsPage.waitForURL('https://adventjs.dev')
-  console.log('Ingreso satisfactorio, recuperando data');
+  console.log('Ingreso satisfactorio, recuperando data...');
   const data = await obtainData(adventJsPage)
   console.log('Data recuperada');
-  console.log(data);
-  await browser.close()
+  browser.close()
   return data 
 }
 
-getInfo()
+const saveData = async getListOfCodes => {
+  // Trabajar el path con regex
+  console.log('Buscando data...');
+  const listOfCodes = await getListOfCodes() 
+  console.log('Iniciando proceso de creación de código...');
+  const basePath = process.cwd()
+  const promisesForCreateFiles = listOfCodes.map((codeObject, index) => (
+    new Promise(async (resolve) => {
+      index = index < 9 ? `0${index + 1}` : `${index + 1}` 
+      const { code } = codeObject
+      const path = `${basePath}/src/adventjs-challenges/${index}`
+      // Creamos el directorio
+      try {
+        await mkdir(path)
+      } catch (error) {
+        console.log('Ya existe el directorio ', path);
+      }
+      // Creamos al archivo que contiene la solución
+      appendFileSync(
+        `${path}/${index}.solution.js`,
+        code
+      )
+      // Creamos el template para tu solución
+      const headerCode = code.split('\n')[0]
+      const yourSolveCode = `${headerCode}\n` +
+      '  // your solution\n' +
+      '}' 
+      appendFileSync(
+        `${path}/${index}.your-code.js`,
+        yourSolveCode
+      )
+      resolve()
+    })
+  ))
+  await Promise.all(promisesForCreateFiles)
+  console.log('Ficheros creados exitosamente.');
+}
+
+saveData(getInfo)
